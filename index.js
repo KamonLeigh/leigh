db724dca6bf8d1b54257054c3d8b0930c0f4eba9
System Administrator <root@Shans-MacBook-Pro-2.local>
2020-12-16 01:40:45 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+console.log(`
+    Hi!
+    
+    Byron Dunkley
+
+    I am a fullstack developer from London :-)
+
+    Github: https://github.com/KamonLeigh
+`);
+
+//NB chmod +x index.js
