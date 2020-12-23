37cc923b99d60e1be53006fbeca8f86ed11050d3
System Administrator <root@Shans-MacBook-Pro-2.local>
2020-12-23 03:47:29 +0000

    added init fn and remove from global state

--- index.js
+++ index.js
@@ -1,53 +1,35 @@
 #!/usr/bin/env node
 
-const welcome = require('cli-welcome');
-const pkgJSON = require('./package.json');
-const chalk = require('chalk');
-const checkNode = require('cli-check-node');
-const unhandled = require('cli-handle-unhandled');
+const pkgJSON = require("./package.json");
+const chalk = require("chalk");
 const log = console.log;
-const githubColour = chalk.hex(`#6cc644`).bold.inverse
+const githubColour = chalk.hex(`#6cc644`).bold.inverse;
 const dim = chalk.dim;
-const italic = chalk.italic
-
+const italic = chalk.italic;
+const init = require("./utils/init");
 // Alerts
-const sym = require('log-symbols');
+const sym = require("log-symbols");
 const success = chalk.green.inverse;
 const info = chalk.blue.inverse;
-const warning = chalk.keyword('orange').inverse;
+const warning = chalk.keyword("orange").inverse;
 const error = chalk.red.bold.inverse;
 
-checkNode('9')
-
-// process.on('unhandledRejection', (err) => {
-//     console.log(`unhandledRejection`, err)
-// })
-
-unhandled();
-
-welcome({
-    title: `Byron Dunkley`,
-    tagLine: `Nice to meet you!`,
-    description: pkgJSON.description,
-    version: pkgJSON.version,
-    bgColor: `#6937ff`,
-    color: `#000000`,
-    clear: true,
-    bold: true
-});
-
-
-
-log(`
+(() => {
+  // process.on('unhandledRejection', (err) => {
+  //     console.log(`unhandledRejection`, err)
+  // })
+  init();
+  log(
+      `
     Hi!
-
+        
     ${italic(`I am a fullstack developer from London :-)`)}
-
+        
     ${githubColour(` Github `)} ${dim(`https://github.com/KamonLeigh`)}
-`);
+        `);
 
-console.log(`
- ${sym.success} ${success(` SUCCESS `)} Thanks for checking out my cli
-`);
-
+  console.log(`
+    ${sym.success} ${success(` SUCCESS `)} Thanks for checking out my cli
+        `);
+})();
 //NB chmod +x index.js
