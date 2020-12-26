c3aac2396940cddd219a946f76f1def324f1013d
System Administrator <root@Shans-MacBook-Pro-2.local>
2020-12-26 20:44:35 +0000

    add no-social

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
-const pkgJSON = require("./package.json");
+const handleError = require('cli-handle-error')
 const chalk = require("chalk");
 const log = console.log;
 const githubColour = chalk.hex(`#6cc644`).bold.inverse;
 const dim = chalk.dim;
 const italic = chalk.italic;
 const init = require("./utils/init");
+const args = process.argv.slice(2);
+
 // Alerts
 const sym = require("log-symbols");
 const success = chalk.green.inverse;
@@ -14,18 +16,28 @@ const info = chalk.blue.inverse;
 const warning = chalk.keyword("orange").inverse;
 const error = chalk.red.bold.inverse;
 
+const socialInfo = `${githubColour(` Github `)} ${dim(`https://github.com/KamonLeigh`)}`;
+
+const printSocial = args.indexOf('--no-social') === -1;
+const social = printSocial ? socialInfo : ``;
+
+
+
 (() => {
   // process.on('unhandledRejection', (err) => {
   //     console.log(`unhandledRejection`, err)
   // })
   init();
+
+//   const error = new Error(`something went wrong`);
+
+//   handleError(`CUSTOM ERROR`, error);
   log(
       `
     Hi!
         
     ${italic(`I am a fullstack developer from London :-)`)}
-        
-    ${githubColour(` Github `)} ${dim(`https://github.com/KamonLeigh`)}
+    ${social}
         `);
 
   console.log(`
